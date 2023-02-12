04313eeac2ee5ac7e01d534fe547ceb6048c1bc2
DanaAloqily <danaaloqily@hotmail.com>
2023-02-12 23:05:31 +0300

    www

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0", //to be changed aws
+  apiHost: "ugagram-api-dev.eba-nzqkxhge.us-east-1.elasticbeanstalk.com/api/v0", //to be changed aws
 };
 
 /*
